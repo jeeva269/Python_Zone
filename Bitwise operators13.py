3c685017678d162b34bbffd10342bf4d194d36e6
jeeva269 <jeeva18623@gmail.com>
2024-08-22 16:48:17 +0530

    operators

---
+++ Bitwise operators13.py
@@ -0,0 +1,18 @@
+# Bitwise operators...
+'''
+&  AND
+|  OR
+^  XOR
+~  NOT
+<< Zero fill left shift
+>> Singned right shift
+'''                           
+
+a = 25
+b = 45 
+print(a & b)
+print(a | b)
+print(a ^ b)
+print(~a)
+print(a << 2)
+print(a >> 2)
---
+++ Logical operators12.py
@@ -0,0 +1,7 @@
+#Logical operators in python...
+
+
+a = 25
+print(a >= 10 and a <= 20)
+print(a >= 10 or a <= 10)
+print(not(a >= 10 and a <=20))
